bc21145f19a2a40b50c3cecababa757ea3c7e404
Danny-m08 <33608567+Danny-m08@users.noreply.github.com>
2018-06-27 15:40:46 -0400

    Delete connect.php

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	$connection = mysqli_connect("127.0.0.1","root","stevefrench","users");
-
-	if($connection->connect_errno){
-		echo "Failed to connect to database...\n";
-				exit();
-		}
-	 ?>
